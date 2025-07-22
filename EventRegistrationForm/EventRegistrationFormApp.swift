461487c7e60e68a54800105be2689a2d4600f6ff
Francisco Casillas <chiccasillas@gmail.com>
2025-07-22 09:50:46 -0600

    Initial Commit

---
+++ EventRegistrationForm/EventRegistrationFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventRegistrationFormApp.swift
+//  EventRegistrationForm
+//
+//  Created by Francisco Jean on 22/07/25.
+//
+
+import SwiftUI
+
+@main
+struct EventRegistrationFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
